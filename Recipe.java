6a8285e8f704e9af254477d0788a3f60d275add6
Roman <76904779+Mr-R0m4n@users.noreply.github.com>
2021-01-19 23:15:01 +0100

    update

--- Recipe.java
+++ Recipe.java
@@ -4,55 +4,62 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Recipe {
     //Attributes
 
     private String recipe;
     private ArrayList<String> recipeArrayList;
-    private ArrayList<String> shoppinglist;
+    private ArrayList<String> shoppinglist = new ArrayList<>();
+    private ArrayList<String> printlist = new ArrayList<>();
+    private final String shopinglistPath = "D:\\Programmieren\\IdeaProjects\\Einkaufsliste\\src\\einkaufsliste\\Einkaufsliste.txt";
 
 
     //Constructors
 
     //Methods
     public void addRecipe() throws IOException {
-        Scanner scannerAdd = new Scanner(System.in);
-        String[][] recipeArray;
-        String fileName = null;
-        System.out.println("Welches Rezept möchtest Du deiner Einkaufsliste hinzufügen?");
-        System.out.println("Spaghetti Bolognese     1");
-        System.out.println("Gebratener Reis         2");
-        System.out.println("Quarkbrot               3");
-        switch (scannerAdd.nextInt()) {
-            case 1 -> {
-                fileName = "\\SpaghettiBolognese.txt";
+        boolean loop = true;
+        while (loop) {
+            Scanner scannerAdd = new Scanner(System.in);
+            String fileName = null;
+            System.out.println("Welches Rezept möchtest Du deiner Einkaufsliste hinzufügen?");
+            System.out.println("Spaghetti Bolognese     1");
+            System.out.println("Gebratener Reis         2");
+            System.out.println("Quarkbrot               3");
+            switch (scannerAdd.nextInt()) {
+                case 1 -> {
+                    fileName = "\\SpaghettiBolognese.txt";
+                }
+                case 2 -> {
+                    fileName = "\\GebratenerReis.txt";
+                }
+                case 3 -> {
+                    fileName = "\\Quarkbrot.txt";
+                }
             }
-            case 2 -> {
-                fileName = "\\GebratenerReis.txt";
-            }
-            case 3 -> {
-                fileName = "\\Quarkbrot.txt";
-            }
-        }
-        this.recipe = "D:\\Programmieren\\IdeaProjects\\Einkaufsliste\\src\\einkaufsliste\\Rezepte" + fileName;
-        this.recipeArrayList = new ArrayList<>(Files.readAllLines(Paths.get(recipe)));
-        recipeArray = new String[recipeArrayList.size()][2];
-        for (int i = 0; i < this.recipeArrayList.size(); i++) {
-            recipeArray[i][0] = this.recipeArrayList.get(i).substring(0,2).trim();
-            recipeArray[i][1] = this.recipeArrayList.get(i).substring(2).trim();
-        }
-        for (int i = 0; i < this.recipeArrayList.size(); i++) {
-            for (int j = 0; j < 2; j++){
-                System.out.print(recipeArray[i][j] + " ");
+            this.recipe = "D:\\Programmieren\\IdeaProjects\\Einkaufsliste\\src\\einkaufsliste\\Rezepte" + fileName;
+            this.recipeArrayList = new ArrayList<>(Files.readAllLines(Paths.get(recipe)));
+            this.shoppinglist.addAll(recipeArrayList);
+            Collections.sort(this.shoppinglist);
+            System.out.println();
+            System.out.println("Möchtest du ein weiteres Rezept hinzufügen?");
+            System.out.println("1 = Ja");
+            System.out.println("2 = Nein");
+            System.out.println();
+            //Exit loop
+            if (scannerAdd.nextInt() != 1) {
+                loop = false;
             }
             System.out.println();
-            System.out.print(recipeArrayList.get(i));
-            System.out.println();
         }
-        System.out.println(recipeArrayList);
+        for (int i = 0; i < this.shoppinglist.size(); i++) {
+            this.printlist.add(i, this.shoppinglist.get(i).substring(2).trim());
+        }
+        Files.write(Paths.get(this.shopinglistPath),this.printlist);
     }
 
     //Create new recipe
